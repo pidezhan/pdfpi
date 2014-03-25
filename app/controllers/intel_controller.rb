b481392b7e8addd41f2e01e5ed657d0fe50a3776
pidezhan <pidezhan@gmail.com>
2014-03-26 01:05:27 +0800

    fixing pg error

--- app/controllers/intel_controller.rb
+++ app/controllers/intel_controller.rb
@@ -6,7 +6,7 @@ class IntelController < ApplicationController
     
     jobs_OK_by_day = Job.where(:created_at => @start_time.beginning_of_day..Time.zone.now.end_of_day, :status => 'OK').
                         group("date(created_at)").
-                        select("created_at, count(*) as job_count")
+                        select("date(created_at) as created_at, count(*) as job_count")
     @job_OK_count_data = (@start_time.to_date..Date.today).map do |date|
           job = jobs_OK_by_day.detect { |job| job.created_at.to_date == date }
           job && job.job_count.to_f || 0
@@ -14,7 +14,7 @@ class IntelController < ApplicationController
         
     jobs_error_by_day = Job.where(:created_at => @start_time.beginning_of_day..Time.zone.now.end_of_day, :status => 'ERROR').
                         group("date(created_at)").
-                        select("created_at, count(*) as job_count")
+                        select("date(created_at) as created_at, count(*) as job_count")
     @job_error_count_data = (@start_time.to_date..Date.today).map do |date|
           job = jobs_error_by_day.detect { |job| job.created_at.to_date == date }
           job && job.job_count.to_f || 0
