955a8ffb4d02f65c43c05c1f778524f0c09ca951
pidezhan <pidezhan@gmail.com>
2014-11-11 22:25:23 +0800

    revised keep house job to limit to the 1000 records only to solve the CPU issue

--- app/models/job.rb
+++ app/models/job.rb
@@ -9,20 +9,20 @@ class Job < ActiveRecord::Base
     upload_too_old = 2.days.ago
     download_too_old = 2.days.ago
     
-    @uploads = Upload.where('created_at < ?', upload_too_old)
+    @uploads = Upload.where('created_at < ?', upload_too_old).reorder('created_at desc').take(1000)
     @uploads.each do |upload|
       file_path = File.join(Rails.root, 'public', 'system', 'uploads', upload.id.to_s)
       `rm -rf #{file_path}`
-      logger.debug "#{file_path} removed"
-      puts "#{file_path} removed"
+      # logger.debug "#{file_path} removed"
+      # puts "#{file_path} removed"
     end
     
-    @jobs = Job.where('created_at < ?', download_too_old)
+    @jobs = Job.where('created_at < ?', download_too_old).reorder('created_at desc').take(1000)
     @jobs.each do |job|
       file_path = File.join(Rails.root, 'public', 'system', 'downloads', job.session_id)
       `rm -rf #{file_path}`
-      logger.debug "#{file_path} removed"
-      puts "#{file_path} removed"
+      # logger.debug "#{file_path} removed"
+      # puts "#{file_path} removed"
     end
   end
   
